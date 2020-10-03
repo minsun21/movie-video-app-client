804b3ad6aa47764fc4e85062150c378baf3479d0
minsun21 <mspark0429@daum.net>
2020-10-03 22:30:06 +0900

    add redux

--- README.md
+++ README.md
@@ -6,3 +6,7 @@
 
 
 # 2. Redux
+- create store
+- Middle ware
+- dev tools
+-- add proxy 
--- src/index.js
+++ src/index.js
@@ -6,11 +6,16 @@ import { Provider } from 'react-redux';
 import { applyMiddleware, createStore } from 'redux';
 import promiseMiddleware from 'redux-promise';
 import ReduxThunk from 'redux-thunk';
+import Reducer from './reducers/index';
+
 
 const createStoreWithMiddleware = applyMiddleware(promiseMiddleware, ReduxThunk)(createStore)
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware}>
+  <Provider store={createStoreWithMiddleware(Reducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ &&
+    window.__REDUX_DEVTOOLS_EXTENSEION__()
+  )}>
     <React.StrictMode>
       <App />
     </React.StrictMode>
